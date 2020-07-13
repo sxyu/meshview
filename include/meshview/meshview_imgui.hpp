eb9d240a8885c9fea31abd426aa6ca26c1439f62
Alex Yu <sxyu@berkeley.edu>
2020-07-12 20:11:16 -0700

    Include common.hpp in meshview_imgui to make it work independently of meshview.hpp

--- include/meshview/meshview_imgui.hpp
+++ include/meshview/meshview_imgui.hpp
@@ -2,6 +2,8 @@
 #ifndef MESHVIEW_IMGUI_FA8950B7_4B5D_4B58_A95A_46172CD48B21
 #define MESHVIEW_IMGUI_FA8950B7_4B5D_4B58_A95A_46172CD48B21
 
+#include "meshview/common.hpp"
+
 #ifdef MESHVIEW_IMGUI
 #define IMGUI_IMPL_OPENGL_LOADER_GLEW
 #include "meshview/imgui/imgui_impl_opengl3.h"
